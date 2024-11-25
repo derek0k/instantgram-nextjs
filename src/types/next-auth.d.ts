3e2eca61970de570dbfc8b95911f0ae0927bf3be
derek0k <derek0kim@gmail.com>
2024-11-25 21:27:22 +0900

    refactor: User 타입 재정의

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,8 +2,6 @@ import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      username: string;
-    } & DefaultSession["user"];
+    user: User;
   }
 }
