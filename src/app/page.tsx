9d1ff7ce6dab105e1614d6a871baaba2c15e3b21
derek0k <derek0kim@gmail.com>
2024-11-25 21:27:48 +0900

    feat: SideBar 골격 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1 className="text-gray-300">Instantgram</h1>;
+import { auth } from "@/auth";
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+import { redirect } from "next/navigation";
+
+export default async function HomePage() {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/signin");
+  }
+
+  return (
+    <section>
+      <FollowingBar />
+      <PostList />
+      <SideBar user={user} />
+    </section>
+  );
 }
