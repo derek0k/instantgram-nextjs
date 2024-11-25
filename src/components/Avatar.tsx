63faf430a79eddd223a717eef15eafcdf30061a9
derek0k <derek0kim@gmail.com>
2024-11-25 22:55:34 +0900

    feat: Avatar 컴포넌트를 사이즈와 하이라이트 조건에 따라 컴포넌트화

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,15 +1,39 @@
-type Props = { image?: string | null };
+type Props = {
+  image?: string | null;
+  size?: "small" | "normal";
+  highlight?: boolean;
+};
 
-export default function Avatar({ image }: Props) {
+export default function Avatar({
+  image,
+  size = "normal",
+  highlight = false,
+}: Props) {
   return (
-    <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
+    <div className={getContainerStlye(size, highlight)}>
       <img
-        className="rounded-full p-[0.1rem]"
+        className={`bg-white rounded-full p-[0.1rem] ${getImageSizeStyle(
+          size
+        )}`}
         alt="user profile"
         src={image ?? undefined}
-        // 액스박스 방지
         referrerPolicy="no-referrer"
       />
     </div>
   );
+}
+
+function getContainerStlye(size: string, highlight: boolean): string {
+  const baseStyle = "rounded-full flex justify-center items-center"; //w-9 h-9
+  const highlightStyle = highlight
+    ? "bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300"
+    : "";
+  const sizeStyle = size === "small" ? "w-9 h-9" : "w-[68px] h-[68px]";
+  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+}
+
+function getImageSizeStyle(size: string): string {
+  return size === "small"
+    ? "w-[34px] h-[34px] p-[0.1rem]"
+    : "w-16 h-16 p-[0.2rem]";
 }
