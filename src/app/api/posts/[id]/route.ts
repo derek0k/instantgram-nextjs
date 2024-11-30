477f8ebdf1a63ae824969dc04caff3a3eac9db62
derek0k <derek0kim@gmail.com>
2024-11-30 23:29:18 +0900

    feat: Post ID에 따른 디테일 데이터를 가져오는 라우트 핸들러 구현

---
+++ src/app/api/posts/[id]/route.ts
@@ -0,0 +1,19 @@
+import { auth } from "@/auth";
+import { getPost } from "@/service/posts";
+import { NextRequest, NextResponse } from "next/server";
+
+type Context = {
+  params: { id: string };
+};
+
+export async function GET(request: NextRequest, context: Context) {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+
+  return getPost(context.params.id) //
+    .then((data) => NextResponse.json(data));
+}
