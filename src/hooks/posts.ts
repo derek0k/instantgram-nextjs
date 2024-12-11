4d0fa9aa2a28fee2375c41c83f939a14b460ce2a
derek0k <derek0kim@gmail.com>
2024-12-11 20:52:36 +0900

    fix: chore: 사용하지 않는 변수 제거

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -17,7 +17,7 @@ async function addComment(id: string, comment: string) {
   }).then((res) => res.json());
 }
 
-export default function usePosts(cacheKey: string = "api/posts") {
+export default function usePosts() {
   const cacheKeys = useCacheKeys();
   const {
     data: posts,
